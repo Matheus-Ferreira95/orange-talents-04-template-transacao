6245e2b0d2bc066ec9e4ecedc2aea69de3cc1ecd
= <=>
2021-05-24 22:05:05 -0300

    alteração kafka

--- src/main/java/br/com/zupacademy/matheus/transacao/transacao/ListenerDeTransacao.java
+++ src/main/java/br/com/zupacademy/matheus/transacao/transacao/ListenerDeTransacao.java
@@ -2,7 +2,10 @@ package br.com.zupacademy.matheus.transacao.transacao;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +16,11 @@ public class ListenerDeTransacao {
 
     public ListenerDeTransacao(TransacaoRepository transacaoRepository) {
         this.transacaoRepository = transacaoRepository;
+    }
+
+    @Bean
+    JsonMessageConverter jsonmessageConverter(){
+        return new JsonMessageConverter();
     }
 
     @KafkaListener(topics = "${spring.kafka.topic.transactions}")
